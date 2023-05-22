2c23dad2a84d1195ec25e0ccb87c998dbae5744a
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-05-22 07:22:17 -0700

    push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,28 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, coinbaseWallet, localWallet, metamaskWallet, paperWallet } from "@thirdweb-dev/react";
+import { BaseGoerli } from "@thirdweb-dev/chains";
+
+
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+//const activeChain = BaseGoerli;
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider activeChain={activeChain}
+    supportedWallets={[
+    paperWallet({
+      clientId: '675a1c42-9260-42c4-9d00-23aaaf3c4080',
+    }),
+    coinbaseWallet(),
+    metamaskWallet(),
+    localWallet(),
+    
+  ]}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
@@ -10,43 +10,26 @@ const Home: NextPage = () => {
           Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
         </h1>
 
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
         <div className={styles.connect}>
           <ConnectWallet />
         </div>
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
+        <div className={styles.connect}>
+          <Web3Button
+          contractAddress={"0x7Fc61eBCc06e324f04Ad2Fe0d25C44Ae79101208"}
+          //action={(contract) => contract.erc721.claim(1)}
+          action={(contract) => {
+            contract.erc1155.lazyMint(1, "ipfs://Qmcny3J5yGpWjJsvR92DQAZcHYWLDep6GdgdKJTRxU1qyo", "0x0");
+          }}
+          onSuccess={() => alert("Claimed!")}
+          onError={(error) => alert(error.message)}
           >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+            Mint
+          </Web3Button>
         </div>
+
+
+        
       </main>
     </div>
   );
