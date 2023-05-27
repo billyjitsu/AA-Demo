d39e3a38304480790a03977f1d961ccbdb43f924
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-05-26 18:04:18 -0700

    update to instructions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
+// Import tools for wallets and the provider for the wallets
 import { ThirdwebProvider, coinbaseWallet, localWallet, metamaskWallet, paperWallet, smartWallet } from "@thirdweb-dev/react";
 import { API_KEY, FACTORY_ADDRESS, CLIENT_ID } from "../constants/addresses";
+// Import chain support
 import { BaseGoerli } from "@thirdweb-dev/chains";
 
 
@@ -9,23 +11,29 @@ import "../styles/globals.css";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-//const activeChain = BaseGoerli;
- const activeChain = "mumbai";
+const activeChain = BaseGoerli;
+ //const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChain}
     supportedWallets={[
     smartWallet({
+      //Address of the Smart account factory you deployed
       factoryAddress: FACTORY_ADDRESS,
+      //Your third web API Key you generated
       thirdwebApiKey: API_KEY,
+      // You will be covering the gas for the user
       gasless: true,
+      //Adding options for wallets to be used in the smart wallet
+      //Paperwallet is an exteranl wallet option that requires a clientId
       personalWallets: [coinbaseWallet(), metamaskWallet(), localWallet(), paperWallet({
         clientId: CLIENT_ID,
       }), ],
 
     }),
 
+    //Wallets can also sit externally of the smart wallet (pay their own gas)
     paperWallet({
       clientId: CLIENT_ID,
     }),
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import {
+  // Ability to connect all wallets
   ConnectWallet,
+  // Built in hook to call functions to the blockchain
   Web3Button,
+  // Get the address of the wallet connected
   useAddress,
+  // Get the contract info from the addres provided
   useContract,
-  useNFT,
+  // Hook that will allow us to read the content of the wallet
   useOwnedNFTs,
+  // Tool used to display images/media from the blockchain
   MediaRenderer,
 } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
@@ -14,9 +19,9 @@ import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  // Use hooks to get values
   const address = useAddress();
   const { contract } = useContract(NFT_ADDRESS);
- // const { data } = useNFT(contract, 0); //Data of tokenID
   const { data: ownedNFTs, isLoading: loadingOwnedNFTs } = useOwnedNFTs(
     contract,
     address
@@ -28,68 +33,75 @@ const Home: NextPage = () => {
   }, [address, ownedNFTs]);
 
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">BASE</a>!
+    <div className="bg-black min-h-screen  p-4">
+      <main className="flex flex-col items-center text-center justify-center">
+        <h1 className="text-5xl text-white font-bold md:text-7xl leading-tighter tracking-tighter mt-8 md:mt-24 mb-3">
+          Welcome
+        </h1>
+        <h1 className="text-3xl text-white font-bold md:text-5xl leading-tighter tracking-tighter mb-4">
+          Claim your NFT on{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-blue-200">
+            BASE
+          </span>
         </h1>
 
-        <div >
+        <div>
           <ConnectWallet />
         </div>
 
-        <div >
+        <div className="flex flex-col items-center space-y-4 mt-8">
           {!loadingOwnedNFTs &&
             ownedNFTs &&
             ownedNFTs.map((nft, index) => (
               <>
-                
-                <MediaRenderer
+                {/* Display NFTs in Wallet */}
+                <MediaRenderer className="w-1/4 md:w-full"
                   key={index}
                   src={nft.metadata.image}
-                  className={styles.nftImage}
                 />
 
                 <input
                   type="text"
                   value={transferAddress}
                   onChange={(e) => setTransferAddress(e.target.value)}
-                  placeholder="Enter address to transfer NFT"
-                  className={styles.inputField}
+                  placeholder="Enter address"
+                  className="text-center"
                 />
-                { transferAddress && transferAddress.length > 0 && (
-                <Web3Button
-                  contractAddress={NFT_ADDRESS}
-                  action={(contract) =>
-                    contract.erc1155.transfer(
-                      transferAddress,
-                      nft.metadata.id,
-                      1
-                    )
-                  }
-                  onSubmit={() => setTransferAddress("")}
-                  onError={(error) => alert(error.message)}
-                >
-                  Transfer
-                </Web3Button>
-                )}
+                  {/* Hook to run a transfer function on your contract.  Connects to contract.typeofcontract.function(with function parameters) */}
+                  {/* In this case, we are sending to the address set in the input, the tokenID of the NFT, the amount of 1 */}
+                  <Web3Button
+                    contractAddress={NFT_ADDRESS}
+                    action={(contract) =>
+                      contract.erc1155.transfer(
+                        transferAddress,
+                        nft.metadata.id,
+                        1
+                      )
+                    }
+                    onSubmit={() => setTransferAddress("")}
+                    onError={(error) => alert(error.message)}
+                  >
+                    Transfer
+                  </Web3Button>
+              
               </>
             ))}
 
-    
-
           {address && (!ownedNFTs || ownedNFTs.length < 1) && (
+            <>
+            {/* Hook to run a claim function on your contract.  Connects to contract.typeofcontract.function(with function parameters) */}
+            {/* In this case, we are claiming tokenID 0 and recieving 1 of them */}
             <Web3Button
               contractAddress={NFT_ADDRESS}
               action={(contract) => {
                 contract.erc1155.claim(0, 1);
               }}
-              
               // onSuccess={() => alert("Claimed!")}
               onError={(error) => console.log(error.message)}
             >
               Mint
             </Web3Button>
+            </>
           )}
         </div>
       </main>
