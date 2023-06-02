2645ae03401c5970dd2e9d23940e6598c19e8682
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-06-02 12:52:29 -0700

    update for error

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -1,4 +1,4 @@
-export const NFT_ADDRESS = process.env.NEXT_PUBLIC_NFT_ADDRESS;
-export const FACTORY_ADDRESS = process.env.NEXT_PUBLIC_FACTORY_ADDRESS;
-export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-export const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+export const NFT_ADDRESS = process.env.NEXT_PUBLIC_NFT_ADDRESS  || '0x0';
+export const FACTORY_ADDRESS = process.env.NEXT_PUBLIC_FACTORY_ADDRESS || '0x0';
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY || '0x0';
+export const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '0x0';
